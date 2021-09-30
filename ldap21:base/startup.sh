d88fc9f021097bca365e6f605b8a8c97e92e2a5e
edtasixm11 <edtasixm11@gmail.com>
2021-09-30 18:07:58 +0200

    prova

--- ldap21:base/startup.sh
+++ ldap21:base/startup.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
-rm -rf /etc/ldap/slapd.d
-rm -rf /var/lib/ldap
+apt-get -y install slapd
+rm -rf /etc/ldap/slapd.d/*
+rm -rf /var/lib/ldap/*
 slaptest -f /opt/docker/slapd.conf -F /etc/ldap/slapd.d
 slapadd  -F /etc/ldap/slapd.d -l /opt/docker/organitzacio-edt.org.ldif
 chown -R openldap.openldap /etc/ldap/slapd.d /var/lib/ldap
-/usr/sbin/slapd
+/usr/sbin/slapd -d0
+
+
+# Pendent:
+# configuració client
+# posar totes les dades en una injecció massiva inicial
+
