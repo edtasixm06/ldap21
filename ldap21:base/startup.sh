71a495f028d7993c6b0d04d98fad3bd3fa315539
edtasixm06 <edtasixm06@gmail.com>
2021-10-05 10:28:59 +0200

    data: organitzacio usuaris mes

--- ldap21:base/startup.sh
+++ ldap21:base/startup.sh
@@ -7,8 +7,11 @@ rm -rf /etc/ldap/slapd.d/*
 rm -rf /var/lib/ldap/*
 slaptest -f /opt/docker/slapd.conf -F /etc/ldap/slapd.d
 slapadd  -F /etc/ldap/slapd.d -l /opt/docker/organitzacio-edt.org.ldif
+slapadd  -F /etc/ldap/slapd.d -l /opt/docker/usuaris-edt.org.ldi
+slapadd  -F /etc/ldap/slapd.d -l /opt/docker/usuaris-mes-edt.org.ldif
+
 chown -R openldap.openldap /etc/ldap/slapd.d /var/lib/ldap
-/usr/sbin/slapd -d0
+/usr/sbin/slapd -d0 
 
 
 # Pendent:
